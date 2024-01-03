fcef34a77d02bf3e57a5941faa38530492764a7f
Jackiechan142 <longvt.ba12-110@st.usth.edu.vn>
2024-01-03 08:28:36 +0700

    remove Begin.py

---
+++ 1.student.mark.py
@@ -0,0 +1,10 @@
+n = int( input("Number of Student: "))
+for i in range(n):
+    id = input("ID: ")
+    name = input(" Name:  ")
+    Dob = input("DoB: ")
+    thistuples =(id, name, Dob)
+for i in range(n):
+    print(thistuples)
+
+
--- Begin.py
+++
@@ -1,3 +0,0 @@
-print("Who are you?");
-print("I don't care!")
-
