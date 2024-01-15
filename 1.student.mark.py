10b04c9bcb298176981067e8c23348f925b2ba59
Jackiechan142 <longvt.ba12-110@st.usth.edu.vn>
2024-01-15 20:43:15 +0700

    Update code, creat a list table mark, Not finish.

--- 1.student.mark.py
+++ 1.student.mark.py
@@ -24,21 +24,61 @@ def input_numberofcoures():
 # add infromation to each course
 def input_course(number):
      course = {
-          "ID: ": input("ID: "),
+          "ID": input("ID: "),
           "Name": input("Name: ")
      }
      return course
+
+# create mark table for each coure
+def crearte_marktable(number):
+     idstudent = input("ID: ")
+     namestudent = input("Name: ")
+     resultstudent = input("Mark: ")
+     markstudent ={
+          "ID ": idstudent,
+          "Name ": namestudent,
+          "Mark": resultstudent
+     }
+     return markstudent
 
 
 
 def main():
     n = input_numberStudent()
     Students =[]
+    Coures = []
+    Table_mark_coures = []
     for i in range(n):
           person= input_infro(i)
           Students.append(person)
     for i in range(n):
-         print(Students[i])
+         print(Students[i].get("ID"))
+    m = input_numberofcoures()
+    for i in range(m):
+         listcourse = input_course(i)
+         Coures.append(listcourse)
+    for i in range(m):
+         print(Coures[i])
+    l = input("ID of course you want to add mark: ")
+    count =0
+    for i in range(m):
+         if (Coures[i].get('ID')==l):
+              Table_mark_coures.append(Coures[i])
+              k= int(input("The number in this class you want to add: "))
+              for j in range(1,k+1):
+                   personmark = crearte_marktable(j)
+                   Table_mark_coures.append(personmark)
+         else: 
+              count = count+1     
+                   
+    if (count == m):
+         print("This class not found.")     
+    print(Table_mark_coures)
+                   
+    
+
+
+          
 
 main()
 
